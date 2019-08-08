d2e93ca416357c043000e36103b23ebc558dfc88
Jalin Wiley <jmt.wiley@gmail.com>
2019-08-07 23:15:49 -0400

    Pieced together jquery to html

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,7 @@
 $('#start').on('click', function () {
     // console.log("issa button");
     // $('#subwrapper').remove();
-
-    for (var i = 0; i < questions.length; i++) {
-        $('#subwrap').append('<h2>' + questions[i].question + '</h2>');
-        for (var j = 0 ; j <questions[i].answers.length; j++) {
-            $("#subwrap").append("<input type='radio' name='question-" + i+"' value='" + questions[i].answers[j]+ "'>" + questions[i] .answers[j])
-        }
-    }
+    game.start()
 })
 
 var questions = [{
@@ -45,3 +39,98 @@ var questions = [{
 }, {
 
 }];
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function () {
+        game.counter--;
+        $('#counter').html(game.counter);
+        if (game.counter <= 0) {
+            console.log("Time is up!");
+            game.done();
+        }
+    },
+    start: function () {
+        timer = setInterval(game.countdown, 1000);
+        $('#subwrap').prepend('<h2> Time Remaining: <span id = "counter">120</span> Seconds</h2>')
+        $('#start').remove();
+
+        for (var i = 0; i < questions.length; i++) {
+            $('#subwrap').append('<h2>' + questions[i].question+'</h2');
+            for (var j = 0; j < questions[i].answers.length; j++){
+                $("#subwrap").append("<input type ='radio' name='question-" +  i+ "' value='" + questions[i].answers[j]+ "'>" + questions[i].answers[j])
+            }
+        }
+
+    },
+    done: function () {
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[0].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[1].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[2].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[3].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[4].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[5].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[6].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[7].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        this.result();
+    },
+    result: function() {
+        clearInterval(timer);
+        $('#subwrap h2').remove();
+        $('#subwrap').html("<h2> All Done!</h2>");
+        $('#subwrap').append("<h3>Correct Answers: " + this.correct +"<h3>");
+        $('#subwrap').append("<h3>Incorrect Answers: " + this.correct +"<h3>");
+        $('#subwrap').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>")
+    }
+}
